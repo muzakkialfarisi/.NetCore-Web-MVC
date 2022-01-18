674e98331b9a4f1e04258b0df92f5a4d42876ba9
muzakkialfarisi <muzakkialfarisi10071999@gmail.com>
2022-01-18 16:49:31 +0700

    insert db and alert

--- ASP_NET/Controllers/CategoryController.cs
+++ ASP_NET/Controllers/CategoryController.cs
@@ -20,9 +20,28 @@ namespace ASP_NET.Controllers
             return View(objCategoryList);
         }
 
+        //get
         public IActionResult Create()
         {
             return View();
+        }
+
+        //post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError(string.Empty, "pesan"); //string empeti bisa disesuaikan degn field yang ada, alertnya pun bakal beda All
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
         }
     }
 }
--- ASP_NET/Models/Category.cs
+++ ASP_NET/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace ASP_NET.Models
 {
@@ -6,9 +7,11 @@ namespace ASP_NET.Models
     {
         [Key] // inisialisasi PK
         public int Id { get; set; }
+
         [Required]
         public string Name { get; set; }
 
+        [DisplayName("Display Order")] //buat ganti in nama di alert
         public int DisplayOrder { get; set; }
 
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
