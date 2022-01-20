8b32c138abe90ac81815f31c226898db14b21a58
muzakkialfarisi <muzakkialfarisi10071999@gmail.com>
2022-01-20 14:42:38 +0700

    implement category repository

---
+++ ASP/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using ASP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASP.DataAccess.Repository.IRepository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        void Update(Category obj);
+
+        void save();
+    }
+}
--- ASP/Repository/IRepository/IRepository.cs
+++ ASP/Repository/IRepository/IRepository.cs
@@ -7,14 +7,12 @@ using System.Threading.Tasks;
 
 namespace ASP.DataAccess.Repository.IRepository
 {
-    internal interface IRepository<T> where T : class
+    public interface IRepository<T> where T : class
     {
         T GetFirstOrDefault(Expression<Func<T, bool>> filter);
         IEnumerable<T> GetAll();
         void Add(T entity);
-
         void Remove(T entity);
-
         void RemoveRange(IEnumerable<T> entity);
     }
 }
